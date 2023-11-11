51a2f7e596c61f5cfabeeb7dac9b1b35acb57793
Jenny Seburn <jennifer.seburn@gmail.com>
2023-11-11 10:10:24 -0500

    display content on error page

--- react/src/routes/ErrorPage/ErrorPage.tsx
+++ react/src/routes/ErrorPage/ErrorPage.tsx
@@ -1,14 +1,27 @@
 import { useClearSides } from "@/utils/common.hooks";
 import { PageHeader } from "@/library/PageHeader";
 import { Scrollbar } from "@/library/Scrollbar";
+import { useRouteError } from "react-router-dom";
+import { useEffect } from "react";
+import { useErrorPageStyles } from "@/routes/ErrorPage";
 
 export function ErrorPage() {
+  const classes = useErrorPageStyles();
+  const error: Error = useRouteError() as Error;
   useClearSides();
+
+  useEffect(() => {
+    console.log("Error Page", error.name);
+  }, [error])
 
   return (
     <>
       <PageHeader title="Error Page" />
-      <Scrollbar>Error content</Scrollbar>
+      <Scrollbar>
+        {error.stack?.split("\n").map((errLine, idx) => (
+          <p key={idx} css={classes.stackLine}>{errLine}</p>
+        ))}
+      </Scrollbar>
     </>
   );
 }
