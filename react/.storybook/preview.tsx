d7c22ebe1dede43298ec306510ccf588d24b100e
Jenny Seburn <jennifer.seburn@gmail.com>
2023-12-01 21:21:35 -0500

    argTypes update

--- react/.storybook/preview.tsx
+++ react/.storybook/preview.tsx
@@ -7,10 +7,6 @@ import results from "../.jest-test-results.json";
 import { Title, Description, Subtitle } from "@storybook/blocks";
 import React from "react";
 import { ImportStatement, PropsTable } from "./blocks";
-import { faRocketLaunch } from "@fortawesome/pro-duotone-svg-icons";
-import { faPaw } from "@fortawesome/pro-solid-svg-icons";
-import { faPotFood } from "@fortawesome/pro-regular-svg-icons";
-import { faGingerbreadMan } from "@fortawesome/pro-thin-svg-icons";
 
 const preview: Preview = {
   parameters: {
@@ -53,52 +49,6 @@ const preview: Preview = {
     withTests({ results }),
   ],
   argTypes: {
-    colorKey: {
-      defaultValue: "standard",
-      options: [
-        "standard",
-        "primary",
-        "secondary",
-        "info",
-        "success",
-        "warning",
-        "error",
-      ],
-      table: {
-        defaultValue: { summary: `"standard"` },
-      }
-    },
-    fontKey: {
-      control: {
-        type: "select",
-        value: "default"
-      },
-      options: ["default", "body", "heading"],
-      table: {
-        defaultValue: { summary: `"default"`}
-      }
-    },
-    onClick: {
-      type: "function",
-    },
-    icon: {
-      control: {
-        type: "select",
-        value: "Rocket",
-      },
-      options: [
-        "Rocket",
-        "Paw",
-        "Food",
-        "Cookie",
-      ],
-      mapping: {
-        Rocket: faRocketLaunch,
-        Paw: faPaw,
-        Food: faPotFood,
-        Cookie: faGingerbreadMan,
-      },
-    },
     id: {
       control: { type: "text" },
       table: {
---
+++ react/src/storybook/argTypes.ts
@@ -0,0 +1,45 @@
+import { faRocketLaunch } from "@fortawesome/pro-duotone-svg-icons";
+import { faPaw } from "@fortawesome/pro-solid-svg-icons";
+import { faPotFood } from "@fortawesome/pro-regular-svg-icons";
+import { faGingerbreadMan } from "@fortawesome/pro-thin-svg-icons";
+
+export const colorKey = {
+  defaultValue: "standard",
+  options: [
+    "standard",
+    "primary",
+    "secondary",
+    "info",
+    "success",
+    "warning",
+    "error",
+  ],
+  table: {
+    defaultValue: { summary: `"standard"` },
+  },
+};
+
+export const fontKey = {
+  control: {
+    type: "select",
+    value: "default",
+  },
+  options: ["default", "body", "heading"],
+  table: {
+    defaultValue: { summary: `"default"` },
+  },
+};
+
+export const icon = {
+  control: {
+    type: "select",
+    value: "Rocket",
+  },
+  options: ["Rocket", "Paw", "Food", "Cookie"],
+  mapping: {
+    Rocket: faRocketLaunch,
+    Paw: faPaw,
+    Food: faPotFood,
+    Cookie: faGingerbreadMan,
+  },
+};
