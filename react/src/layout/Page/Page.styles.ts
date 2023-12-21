889e83dd5e8e7247f555301b958bb8cc8b3d15f1
Jenny Seburn <jennifer.seburn@gmail.com>
2023-12-21 10:23:29 -0500

    setup base page layout

--- react/src/layout/Page/Page.styles.ts
+++ react/src/layout/Page/Page.styles.ts
@@ -1,7 +1,13 @@
 import { css } from "@emotion/react";
 
 export function usePageStyles() {
-  const root = css({});
+  const root = css({
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    overflow: 'hidden',
+  });
 
   return { root };
 }
--- react/src/layout/Page/Page.tsx
+++ react/src/layout/Page/Page.tsx
@@ -1,8 +1,17 @@
 import { usePageStyles } from "@/layout/Page";
+import { Footer } from "@/layout/Footer";
+import { Outlet } from "react-router-dom";
 
 /** The main wrapper for each application page */
 export function Page() {
   const classes = usePageStyles();
 
-  return <div css={classes.root}>Page Wrapper</div>;
+  return (
+    <>
+      <div css={classes.root}>
+        <Outlet />
+      </div>
+      <Footer />
+    </>
+  );
 }
