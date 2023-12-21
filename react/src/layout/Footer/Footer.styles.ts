19cf2d9eb710977d1d2590bd193851d9bba4cb5f
Jenny Seburn <jennifer.seburn@gmail.com>
2023-12-21 10:40:19 -0500

    setup footer

--- react/src/layout/Footer/Footer.styles.ts
+++ react/src/layout/Footer/Footer.styles.ts
@@ -1,7 +1,25 @@
-import { css } from "@emotion/react";
+import { css, useTheme } from "@emotion/react";
 
 export function useFooterStyles() {
-  const root = css({});
+  const theme = useTheme();
 
-  return { root };
+  const root = css({
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    background: theme.colors.standard.altContrast,
+    color: theme.colors.standard.main,
+    fontSize: "0.875rem",
+    padding: `${theme.spacing.medium}px ${theme.spacing.large}px`,
+    "& p": {
+      margin: 0,
+    },
+  });
+
+  const icon = css({
+    marginRight: theme.spacing.small,
+  });
+
+  return { root, icon };
 }
--- react/src/layout/Footer/Footer.tsx
+++ react/src/layout/Footer/Footer.tsx
@@ -1,8 +1,18 @@
+import { Icon } from "@/elements/Icon";
 import { useFooterStyles } from "@/layout/Footer";
+import { faCopyright } from "@fortawesome/pro-regular-svg-icons";
 
 /** The site footer section */
 export function Footer() {
   const classes = useFooterStyles();
 
-  return <footer css={classes.root}>Site Footer</footer>;
+  return (
+    <footer css={classes.root}>
+      <p>
+        <Icon css={classes.icon} icon={faCopyright} />
+        2023 jsburn.com
+      </p>
+      <p>Recipe Site &mdash; React Version</p>
+    </footer>
+  );
 }
