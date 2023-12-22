45e929665d670b6fc74184ec6e12f57af41d10b3
Jenny Seburn <jennifer.seburn@gmail.com>
2023-12-22 14:04:44 -0500

    display recipe summary

--- react/src/components/Summary/Summary.styles.ts
+++ react/src/components/Summary/Summary.styles.ts
@@ -3,7 +3,17 @@ import { css, useTheme } from "@emotion/react";
 export function useSummaryStyles() {
   const theme = useTheme();
 
-  const root = css({});
+  const root = css({
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    overflow: "hidden",
+  });
 
-  return { root };
+  const body = css({
+    margin: `${theme.spacing.medium}px ${theme.spacing.large}px`,
+    fontSize: "0.875rem",
+  });
+
+  return { root, body };
 }
--- react/src/components/Summary/Summary.tsx
+++ react/src/components/Summary/Summary.tsx
@@ -1,8 +1,23 @@
 import { SummaryProps, useSummaryStyles } from "@/components/Summary";
+import uniqueId from "lodash/uniqueId";
+import { Scrollbar } from "@/elements/Scrollbar";
+import { SectionHeader } from "@/elements/SectionHeader";
+import { useAppContext } from "@/layout/App";
 
 /** Render the recipe's summary inside a scrollbar container with an optional header */
 export function Summary(props: SummaryProps) {
+  const { className, style, id = uniqueId("summary-") } = props;
+  const { recipe } = useAppContext();
   const classes = useSummaryStyles();
 
-  return <div css={classes.root}>Summary</div>;
+  if (!recipe?.summary) return null;
+
+  return (
+    <div css={classes.root} className={className} style={style} id={id}>
+      <SectionHeader title="Summary" />
+      <Scrollbar>
+        <p css={classes.body}>{recipe.summary}</p>
+      </Scrollbar>
+    </div>
+  );
 }
